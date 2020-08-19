ddfae4a515259a0c92684807d4f5ceeb49beefbe
Ben Allen <benjaminallen@peralta.edu>
2020-08-18 19:46:26 -0700

    Add project files.

---
+++ helloworld.cpp
@@ -0,0 +1,7 @@
+/* file: helloworld.cpp
+ * --------------------
+ * 
+ * The first C++ short assignment for CIS6. To complete, write a program that uses the iostream
+ * library to print the words "hello world!" to the console
+ */
+
